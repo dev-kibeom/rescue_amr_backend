49d3ddd2c0cf942bdc5ec8c33cf386c4f7a858e1
dev-kibeom <neopkrrl@gmail.com>
2026-06-10 11:24:01 +0900

    rebase

--- database/app/repositories/rescue_amr_repository.py
+++ database/app/repositories/rescue_amr_repository.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.database import db, RescueRobot, IncidentLog
 
 # 터틀봇4 관련 데이터베이스 작업(Querry, Update, Delete)을 담당하는 Repository 클래스
-class Turtlebot4Repository:
+class RescueAmrRepository:
     @staticmethod
     def get_robot_by_id(robot_id: str):
         return RescueRobot.query.filter_by(id=robot_id).first()
@@ -10,12 +10,10 @@ class Turtlebot4Repository:
     @staticmethod
     def save_robot(robot: RescueRobot):
         db.session.add(robot)
-        db.session.commit()
 
     @staticmethod
     def create_log(log: IncidentLog):
         db.session.add(log)
-        db.session.commit()
 
     @staticmethod
     def get_recent_logs(limit: int = 10):
